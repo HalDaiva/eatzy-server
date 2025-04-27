770f6b5352abe33976ad818584bec4e87ee89296
HalDaiva <daivahalilintar@gmail.com>
2025-04-27 17:21:15 +0700

    wow

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'eatzy',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA,
+    port: parseInt(process.env.DATABASE_PORT)
 });
 
 db.connect(err => {
